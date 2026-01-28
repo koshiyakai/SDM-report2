e387b0786118d96f97dcdbd76f63942a48855eff
kai <koshiya.kai.kj@tut.jp>
2026-01-28 16:02:41 +0900

    testcase change(2)

--- test_cases.py
+++ test_cases.py
@@ -8,14 +8,20 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         def test_sample1 (self):
-                self.assertEqual (21, calc(3,7))
-
+                self.assertEqual (1, calc(1,1))
         def test_sample2 (self):
-                self.assertEqual (-1, calc(0,150))
-
+                self.assertEqual (998001, calc(999,999))
         def test_sample3 (self):
-                self.assertEqual (-1, calc('a','b'))
-
+                self.assertEqual (-1, calc(0,500))
         def test_sample4 (self):
-                self.assertEqual (-1, calc(0.1,999))
-
+                self.assertEqual (-1, calc(1000,500))
+        def test_sample5 (self):
+                self.assertEqual (-1, calc(0.1,500))
+        def test_sample6 (self):
+                self.assertEqual (-1, calc("a", 500))
+        def test_sample7 (self):
+                self.assertEqual (-1, calc(None, 500))                            
+        def test_sample8 (self):
+                self.assertEqual (-1, calc(999999999, 500))
+        def test_sample9 (self):
+                self.assertEqual (-1, calc(-999999999, 500))
