f85ee363c8ff50a305da3f5da01f47c83042fd3c
kai <koshiya.kai.kj@tut.jp>
2026-01-28 18:30:25 +0900

    (3) program apdata

--- test_cases.py
+++ test_cases.py
@@ -2,26 +2,25 @@
 
 import unittest
 from calc_mul import calc
-
 # Run with testrunner so needs to be in file test_
 
 class TestCalc (unittest.TestCase):
 
-        def test_sample1 (self):
-                self.assertEqual (1, calc(1,1))
-        def test_sample2 (self):
-                self.assertEqual (998001, calc(999,999))
-        def test_sample3 (self):
+        def test_sample01 (self):
+                self.assertEqual (500, calc(1,500))
+        def test_sample02 (self):
+                self.assertEqual (499500, calc(999,500))
+        def test_sample03 (self):
                 self.assertEqual (-1, calc(0,500))
-        def test_sample4 (self):
+        def test_sample04 (self):
                 self.assertEqual (-1, calc(1000,500))
-        def test_sample5 (self):
+        def test_sample05 (self):
                 self.assertEqual (-1, calc(0.1,500))
-        def test_sample6 (self):
+        def test_sample06 (self):
                 self.assertEqual (-1, calc("a", 500))
-        def test_sample7 (self):
+        def test_sample07 (self):
                 self.assertEqual (-1, calc(None, 500))                            
-        def test_sample8 (self):
-                self.assertEqual (-1, calc(999999999, 500))
-        def test_sample9 (self):
-                self.assertEqual (-1, calc(-999999999, 500))
+        def test_sample08 (self):
+                self.assertEqual (12, calc(3, 4))
+        def test_sample09 (self):
+                self.assertEqual (12, calc(4, 3))                
